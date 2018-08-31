343fbc2cd9b8617cf765b3ebd65725d10f10d9ea
huangke <huangke@samsource.cn>
2018-08-31 17:05:55 +0800

    被限制访问频率了，访问间隔降到每分钟5次以下

--- 一级目录爬虫多线程版.py
+++ 一级目录爬虫多线程版.py
@@ -8,6 +8,8 @@ from pymongo import MongoClient
 conn = MongoClient('127.0.0.1', 27017)
 db = conn.lagou
 
+proxy = {"https": 'https://%s' % input('请输入代理IP: ')}
+
 def get_json(keyword, page):
     data = {
         'pn': page,
@@ -15,21 +17,25 @@ def get_json(keyword, page):
     }
     url = 'https://www.lagou.com/jobs/positionAjax.json?city=%E6%88%90%E9%83%BD&needAddtionalResult=false'
     headers = {
-        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) '
-                      'Chrome/68.0.3440.106 Safari/537.36',
-        'Accept':     'application/json, text/javascript, */*; q=0.01',
-        'Referer':    'https://www.lagou.com/jobs/list_{}?city=%E6%88%90%E9%83%BD&cl=false&fromSearch=true&labe'
-                      'lWords=&suginput='.format(keyword),
-        'Cookie':     '_ga=GA1.2.1176219052.1525516654; user_trace_token=20180505183734-522d0969-5050-11e8-8032-5254005c3644; LGUID=20180505183734-522d0d7e-5050-11e8-8032-5254005c3644; index_location_city=%E6%88%90%E9%83%BD; showExpriedIndex=1; showExpriedCompanyHome=1; showExpriedMyPublish=1; _gid=GA1.2.1609482079.1535252885; JSESSIONID=ABAAABAAAGFABEFAF1B82E55AD78E727FBE8F9A524F11DC; Hm_lvt_4233e74dff0ae5bd0a3d81c6ccf756e6=1535252885,1535388327,1535567308,1535581083; X_HTTP_TOKEN=0a538ca6731db75799ffed14888c0323; LG_LOGIN_USER_ID=1e8c26fb6976688aebb8f4404658cbe533c7012a4bb16eae; _putrc=68BFC909BD7605C8; login=true; unick=%E9%BB%84%E7%A7%91; hasDeliver=138; gate_login_token=9ceb36e4bc23b9210272f2e4722a69b28adc98a79698db16; LGSID=20180831132504-3714cdf5-acde-11e8-be60-525400f775ce; TG-TRACK-CODE=jobs_again; _gat=1; SEARCH_ID=2fa0ee6acb5b4235b444caff55887def; Hm_lpvt_4233e74dff0ae5bd0a3d81c6ccf756e6=1535697322; LGRID=20180831143522-09678973-ace8-11e8-be67-525400f775ce'
+        'Accept':             'application/json, text/javascript, */*; q=0.01',
+        'Content-Type':       'application/x-www-form-urlencoded; charset=UTF-8',
+        'Origin':             'https://www.lagou.com',
+        'Referer':            'https://www.lagou.com/jobs/list_{}?city=%E6%88%90%E9%83%BD&cl=false&fromSearch=true&labelWords=&suginput='.format(
+            keyword),
+        'User-Agent':         'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36',
+        'X-Anit-Forge-Code':  '0',
+        'X-Anit-Forge-Token': 'None',
+        'X-Requested-With':   'XMLHttpRequest'
     }
     try:
-        r = requests.post(url, headers=headers, data=data)
+        r = requests.post(url, headers=headers, data=data, proxies=proxy, timeout=5)
         if r.status_code == 200:
             print(r.json())
             return r.json()
         else:
             print('crawl page %s failed' % page)
     except Exception as e:
+        print(e)
         return
 
 def parse_jd_page(_id):
@@ -94,13 +100,10 @@ def save_to_mongo(data):
         return None
 
 def main(keyword, page):
-    lock.acquire()
     data = get_json(keyword, page)
     parse_jd_page(data)
     data_gne = parse_json(data)
-    lock.release()
     save_to_mongo(data_gne)
-
 
 if __name__ == '__main__':
     keyword = input('要爬取的职位: ')
@@ -111,12 +114,12 @@ if __name__ == '__main__':
 
     threads = []
     for i in range(1, pages + 1):
-        lock = threading.Lock()
         t = threading.Thread(target=main, args=(keyword, i))
         threads.append(t)
 
     for t in threads:
         t.start()
+        time.sleep(15)
 
     for t in threads:
         t.join()
