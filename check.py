6067475a26a036bf2d0104730eb1c283fc74d20e
huangke19 <huangkwell@163.com>
2018-09-04 21:58:39 +0800

    解析规则添加测试

--- check.py
+++ check.py
@@ -8,44 +8,17 @@ def parse_checker(html):
     from bs4 import BeautifulSoup
     soup = BeautifulSoup(html, 'lxml')
 
-    job_name = soup.select_one(".job-name .name").string
-    assert bool(job_name) is True, 'job_name解析规则发生了变化'
-
-    depart_name = soup.select_one(".company").string
-    assert bool(depart_name) is True, 'depart_name解析规则发生了变化'
-
-    city = soup.select(".job_request span")[1].string.strip('/').strip()
-    assert bool(city) is True, '%s解析规则发生了变化' % city
-
-    experience = soup.select(".job_request span")[2].string.strip('/').strip()
-    assert bool(experience) is True, '%s解析规则发生了变化' % experience
-
-    edu = soup.select(".job_request span")[3].string.strip('/').strip()
-    assert bool(edu) is True, '%s解析规则发生了变化' % edu
-
-    work_time = soup.select(".job_request span")[4].string.strip('/').strip()
-    assert bool(work_time) is True, '%s解析规则发生了变化' % work_time
-
-    advantage = soup.select_one(".job-advantage p").string
-    assert bool(advantage) is True, '%s解析规则发生了变化' % advantage
-
-    job_desc = soup.select_one(".job_bt").text
-    assert bool(job_desc) is True, '%s解析规则发生了变化' % job_desc
-
-    addr = ''.join(soup.select_one(".work_addr").text.split()[:-1])
-    assert bool(addr) is True, '%s解析规则发生了变化' % addr
-
-    company = soup.select_one(".job_company h2").text.split()[0]
-    assert bool(company) is True, '%s解析规则发生了变化' % company
-
-    comp_field = soup.select(".c_feature li")[0].text.split()
-    assert bool(comp_field) is True, '%s解析规则发生了变化' % comp_field
-
-    progress = soup.select(".c_feature li")[1].text.split()[0]
-    assert bool(progress) is True, '%s解析规则发生了变化' % progress
-
-    scale = soup.select(".c_feature li")[2].text.split()[0]
-    assert bool(scale) is True, '%s解析规则发生了变化' % scale
-
-    comp_url = soup.select(".c_feature li")[3].text.split()[0]
-    assert bool(comp_url) is True, '%s解析规则发生了变化' % comp_url
+    assert soup.select_one(".job-name .name").string, 'job_name解析规则发生了变化'
+    assert soup.select_one(".company").string, 'depart_name解析规则发生了变化'
+    assert soup.select(".job_request span")[1].string.strip('/').strip(), 'city解析规则发生了变化'
+    assert soup.select(".job_request span")[2].string.strip('/').strip(), 'experience解析规则发生了变化'
+    assert soup.select(".job_request span")[3].string.strip('/').strip(), 'edu解析规则发生了变化'
+    assert soup.select(".job_request span")[4].string.strip('/').strip(), 'work_time解析规则发生了变化'
+    assert soup.select_one(".job-advantage p").string, 'advantage 解析规则发生了变化'
+    assert soup.select_one(".job_bt").text, 'job_desc解析规则发生了变化'
+    assert ''.join(soup.select_one(".work_addr").text.split()[:-1]), 'addr解析规则发生了变化'
+    assert soup.select_one(".job_company h2").text.split()[0], 'company解析规则发生了变化'
+    assert soup.select(".c_feature li")[0].text.split(), 'comp_field解析规则发生了变化'
+    assert soup.select(".c_feature li")[1].text.split()[0], 'progress解析规则发生了变化'
+    assert soup.select(".c_feature li")[2].text.split()[0], 'scale解析规则发生了变化'
+    assert soup.select(".c_feature li")[3].text.split()[0], ' comp_url解析规则发生了变化'
