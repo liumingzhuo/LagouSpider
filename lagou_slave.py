5209c6197f1797f35dc74c7ece9df5910b1069bf
huangke19 <huangkwell@163.com>
2018-09-04 22:50:10 +0800

    slave爬虫

--- lagou_slave.py
+++ lagou_slave.py
@@ -2,18 +2,13 @@
 # -*- coding: utf-8 -*-
 
 ''' 拉勾网全站爬虫 slave节点'''
-import multiprocessing
-import re
-import threading
-import time
-from multiprocessing.pool import Pool
 
+import time
 import redis
 import requests
+import threading
 from bs4 import BeautifulSoup
 from pymongo import MongoClient
-
-from check import parse_checker
 
 HOST = '192.168.31.214'
 PORT = 6379
@@ -66,7 +61,6 @@ def parse_position(url, html):
     使用bs提取职位页面所有需要的信息
     '''
     if html:
-
         parse_checker(url, html)
         # anspider_checker(html)
 
@@ -125,6 +119,39 @@ def save_to_mongo(url, data):
             return None
     else:
         return None
+
+
+''' 检查解析规则是否失效 '''
+from lagou_slave import redis_conn
+
+
+def parse_checker(url, html):
+    from bs4 import BeautifulSoup
+    soup = BeautifulSoup(html, 'lxml')
+
+    try:
+        online = soup.select_one(".outline_tag").string
+        if online == '（该职位已下线）':
+            print('该职位已下线')
+            redis_conn.sadd('crawled_urls', url)
+            return
+    except AttributeError:
+        pass
+
+    assert soup.select_one(".job-name .name").string, 'job_name解析规则发生了变化'
+    assert soup.select_one(".company").string, 'depart_name解析规则发生了变化'
+    assert soup.select(".job_request span")[1].string.strip('/').strip(), 'city解析规则发生了变化'
+    assert soup.select(".job_request span")[2].string.strip('/').strip(), 'experience解析规则发生了变化'
+    assert soup.select(".job_request span")[3].string.strip('/').strip(), 'edu解析规则发生了变化'
+    assert soup.select(".job_request span")[4].string.strip('/').strip(), 'work_time解析规则发生了变化'
+    assert soup.select_one(".job-advantage p").string, 'advantage 解析规则发生了变化'
+    assert soup.select_one(".job_bt").text, 'job_desc解析规则发生了变化'
+    assert ''.join(soup.select_one(".work_addr").text.split()[:-1]), 'addr解析规则发生了变化'
+    assert soup.select_one(".job_company h2").text.split()[0], 'company解析规则发生了变化'
+    assert soup.select(".c_feature li")[0].text.split(), 'comp_field解析规则发生了变化'
+    assert soup.select(".c_feature li")[1].text.split()[0], 'progress解析规则发生了变化'
+    assert soup.select(".c_feature li")[2].text.split()[0], 'scale解析规则发生了变化'
+    assert soup.select(".c_feature li")[3].text.split()[0], ' comp_url解析规则发生了变化'
 
 
 def main():
